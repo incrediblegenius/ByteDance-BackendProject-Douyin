143c8513b851315708a46cd77d0de7d5d672c195
evil <595880227>
2022-05-25 16:52:37 +0800

    增加部署k8s——仓库使用腾讯云

--- comment-srv/global/global.go
+++ comment-srv/global/global.go
@@ -3,45 +3,44 @@ package global
 import (
 	"CommentSrv/cfg"
 	"CommentSrv/middleware"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
-	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/vo"
-	"github.com/spf13/viper"
+	"github.com/namsral/flag"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
-const (
-	NacosFileName = "config.yaml"
-)
-
 var (
-	ServerConfig cfg.ServerConfig
-	NacosConfig  cfg.NacosConfig
-	DB           *gorm.DB
-	Jwt          *middleware.JWT
-	NamingClient naming_client.INamingClient
-	ConfigClient config_client.IConfigClient
+	MysqlInfo   cfg.MysqlConfig
+	DB          *gorm.DB
+	Jwt         *middleware.JWT
+	ServicePort int
 )
 
 func init() {
-	InitNacos()
+	InitParse()
 	InitDB()
 	Jwt = middleware.NewJWT()
 }
 
+func InitParse() {
+	flag.StringVar(&MysqlInfo.User, "mysql_user", "root", "mysql user")
+	flag.StringVar(&MysqlInfo.Password, "mysql_password", "root", "mysql password")
+	flag.StringVar(&MysqlInfo.Host, "mysql_host", "127.0.0.1", "mysql host")
+	flag.IntVar(&MysqlInfo.Port, "mysql_port", 3306, "mysql port")
+	flag.StringVar(&MysqlInfo.Name, "mysql_name", "douyin_user", "mysql name")
+	flag.IntVar(&ServicePort, "service_port", 8080, "service port")
+	flag.Parse()
+}
+
 func InitDB() {
-	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", ServerConfig.MysqlInfo.User, ServerConfig.MysqlInfo.Password, ServerConfig.MysqlInfo.Host, ServerConfig.MysqlInfo.Port, ServerConfig.MysqlInfo.Name)
+	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", MysqlInfo.User, MysqlInfo.Password, MysqlInfo.Host, MysqlInfo.Port, MysqlInfo.Name)
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags),
 		logger.Config{
@@ -61,71 +60,4 @@ func InitDB() {
 		panic(err)
 	}
 	fmt.Println("数据库连接成功")
-}
-
-func InitNacos() {
-	v := viper.New()
-	v.SetConfigFile(NacosFileName)
-	if err := v.ReadInConfig(); err != nil {
-		panic(err)
-	}
-	if err := v.Unmarshal(&NacosConfig); err != nil {
-		panic(err)
-	}
-	sc := []constant.ServerConfig{
-		{
-			IpAddr: NacosConfig.Host,
-			Port:   NacosConfig.Port,
-		},
-	}
-	cc := constant.ClientConfig{
-		NamespaceId:         NacosConfig.Namespace,
-		TimeoutMs:           5000,
-		NotLoadCacheAtStart: true,
-		LogDir:              "nacos/log",
-		CacheDir:            "nacos/cache",
-		LogLevel:            "warn",
-	}
-	var err error
-	ConfigClient, err = clients.NewConfigClient(
-		vo.NacosClientParam{
-			ClientConfig:  &cc,
-			ServerConfigs: sc,
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	NamingClient, err = clients.NewNamingClient(
-		vo.NacosClientParam{
-			ClientConfig:  &cc,
-			ServerConfigs: sc,
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	content, err := ConfigClient.GetConfig(vo.ConfigParam{
-		DataId: NacosConfig.DataId,
-		Group:  NacosConfig.Group})
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal([]byte(content), &ServerConfig)
-	if err != nil {
-		panic(err)
-	}
-	err = ConfigClient.ListenConfig(vo.ConfigParam{
-		DataId: NacosConfig.DataId,
-		Group:  NacosConfig.Group,
-		OnChange: func(namespace, group, dataId, data string) {
-			fmt.Println("group:" + group + ", dataId:" + dataId + "配置发生改变！")
-			err = json.Unmarshal([]byte(data), &ServerConfig)
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-	// fmt.Println(ServerConfig)
-	fmt.Println("成功从nacos读取配置")
 }
